70313a097227018841a0003eae064409bb09d540
hemanth byrisetti <hemanthbyrisetti@Hemanths-MacBook-Pro-2.local>
2020-11-06 00:22:26 +0530

    basic image layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUrls: [],
+      isFetchingData: false,
+    }
+  }
 
-export default App;
+  componentDidMount() {
+    fetch('https://api.unsplash.com/photos', {
+      headers: {
+        'Authorization': 'Client-ID TW5679V-cgynLNfQbJB_TOQsbHDlovsVkwhjgIfh6F0',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        const tempState = [...this.state.imageUrls]
+        this.setState({ imageUrls: [...tempState, ...data], isFetchingData: false });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  render() {
+    const { imageUrls } = this.state;
+    console.log(this.state.imageUrls)
+    return (
+      <div className="App">
+        <section>
+          {imageUrls.map(image => <img key={image.id} src={image.urls.small} alt="img" />)}
+        </section>
+      </div >
+    )
+  }
+}
